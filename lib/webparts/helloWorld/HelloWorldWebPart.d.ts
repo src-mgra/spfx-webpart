bb226083a6a695d33ab16ce8f555b06697d720f2
Micha G <25931746+src-mgra@users.noreply.github.com>
2018-03-26 10:00:26 +0200

    Add files via upload

---
+++ lib/webparts/helloWorld/HelloWorldWebPart.d.ts
@@ -0,0 +1,14 @@
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart, IPropertyPaneConfiguration } from '@microsoft/sp-webpart-base';
+export interface IHelloWorldWebPartProps {
+    description: string;
+    test: string;
+    test1: boolean;
+    test2: string;
+    test3: boolean;
+}
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+    render(): void;
+    protected readonly dataVersion: Version;
+    protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration;
+}
