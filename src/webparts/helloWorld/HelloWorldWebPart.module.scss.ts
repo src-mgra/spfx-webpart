b0a9dba4a65f432bdec0351f30791d7b2cf723c7
Micha G <25931746+src-mgra@users.noreply.github.com>
2018-03-26 09:48:54 +0200

    Add files via upload

---
+++ src/webparts/helloWorld/HelloWorldWebPart.module.scss.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+require('./HelloWorldWebPart.module.css');
+const styles = {
+  helloWorld: 'helloWorld_aeaf6f3d',
+  container: 'container_aeaf6f3d',
+  row: 'row_aeaf6f3d',
+  column: 'column_aeaf6f3d',
+  'ms-Grid': 'ms-Grid_aeaf6f3d',
+  title: 'title_aeaf6f3d',
+  subTitle: 'subTitle_aeaf6f3d',
+  description: 'description_aeaf6f3d',
+  button: 'button_aeaf6f3d',
+  label: 'label_aeaf6f3d',
+};
+
+export default styles;
+/* tslint:enable */
